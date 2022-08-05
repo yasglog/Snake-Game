0a8d499b2cd05663b84d7a37638619760ff8a698
yashgaikwad <yashrgaikwad1235@gmail.com>
2022-08-05 23:32:06 +0530

    create the snake food and snake Head and also display in grid

--- js/index.js
+++ js/index.js
@@ -6,6 +6,10 @@ let gameoversound=new Audio();
 let moviesound =new Audio();
 let musicsound=new Audio();
 let speed=2;
+let snakearray=[
+    {x:2,y:1}
+]
+let food={x:5,y:4}
 
 //function in game
 
@@ -19,13 +23,36 @@ function main(ctime){
    if((ctime-lasttime)/1000<1/speed){
        return;
    }
-//    console.log(ctime)
    lasttime=ctime;
-//    gameEngine();
+   gameEngine();
+
 }
 //divided into two part
 function gameEngine(){
 
+    // part 2 display snake and food 2
+    //display the snake 
+    board.innerHtml="";
+    snakearray.forEach((e,index)=>{
+    selectelement=document.createElement('div');
+    selectelement.style.gridRowStart=e.y;
+    selectelement.style.gridColumnStart=e.x;
+    if (index==0) {
+        selectelement.classList.add('head');
+
+    }
+    else{
+    selectelement.classList.add('snake');
+    }
+
+    board.appendChild(selectelement);
+ 
+    });
+    foodelement=document.createElement('div');
+    foodelement.style.gridRowStart=food.y;
+    foodelement.style.gridColumnStart=food.x;
+    foodelement.classList.add('food');
+    board.appendChild(foodelement);
 }
 window.requestAnimationFrame(main);
 
